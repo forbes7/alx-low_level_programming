f21b04bdb4ee024f1f8aa031d83317ed73b358b7
forbes7 <Emmanuelmiraclechukwukadibia@gmail.com>
2022-04-11 10:20:03 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints a sentence with the printf function
+ *
+ * Return: returns 0 after printing
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
